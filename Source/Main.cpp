1e583b8e609cb4e75ff4d5118c7b00a0479223aa
WAVE <fenix24@naver.com>
2024-02-11 11:08:31 +0900

    [Temp] TMS TEST
     - TMS Test 는 이정도로 하고 다음은 Dev 로 가 보자.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -6,6 +6,23 @@
 #include "Main.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
+#pragma link "AdvToolBar"
+#pragma link "AdvMenus"
+#pragma link "AdvToolBarStylers"
+#pragma link "AdvGrid"
+#pragma link "AdvObj"
+#pragma link "BaseGrid"
+#pragma link "AdvGlowButton"
+#pragma link "AdvSmoothDock"
+#pragma link "INIInspectorBar"
+#pragma link "InspectorBar"
+#pragma link "AdvOfficeComboBox"
+#pragma link "RTTIInspectorBar"
+#pragma link "AdvPanel"
+#pragma link "ToolPanels"
+#pragma link "AdvSmoothExpanderButtonPanel"
+#pragma link "AdvSmoothExpanderPanel"
+#pragma link "AdvSmoothPanel"
 #pragma resource "*.dfm"
 TFormMain *FormMain;
 //---------------------------------------------------------------------------
@@ -24,5 +41,12 @@ void __fastcall TFormMain::InitProgram() {
 
 void __fastcall TFormMain::PrintMsg(UnicodeString _str) {
 	memo->Lines->Add(_str);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TFormMain::btn_TestClick(TObject *Sender)
+{
+	//advdockpn->
+    DockPanel_Top->ToolBarStyler;
 }
 //---------------------------------------------------------------------------
--- Source/Main.h
+++ Source/Main.h
@@ -7,11 +7,55 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include "AdvToolBar.hpp"
+#include "AdvMenus.hpp"
+#include "AdvToolBarStylers.hpp"
+#include "AdvGrid.hpp"
+#include "AdvObj.hpp"
+#include "BaseGrid.hpp"
+#include <Vcl.Grids.hpp>
+#include "AdvGlowButton.hpp"
+#include "AdvSmoothDock.hpp"
+#include "INIInspectorBar.hpp"
+#include "InspectorBar.hpp"
+#include <Vcl.ExtCtrls.hpp>
+#include "AdvOfficeComboBox.hpp"
+#include "RTTIInspectorBar.hpp"
+#include "AdvPanel.hpp"
+#include "ToolPanels.hpp"
+#include "AdvSmoothExpanderButtonPanel.hpp"
+#include "AdvSmoothExpanderPanel.hpp"
+#include "AdvSmoothPanel.hpp"
 //---------------------------------------------------------------------------
 class TFormMain : public TForm
 {
 __published:	// IDE-managed Components
 	TMemo *memo;
+	TAdvDockPanel *DockPanel_Top;
+	TButton *btn_Test;
+	TAdvToolBar *AdvToolBar1;
+	TAdvDockPanel *DockPanel_Right;
+	TAdvToolBar *AdvToolBar2;
+	TAdvToolBarButton *AdvToolBarButton1;
+	TAdvToolBarMenuButton *AdvToolBarMenuButton1;
+	TAdvMenuStyler *AdvMenuStyler1;
+	TAdvToolBarFantasyStyler *AdvToolBarFantasyStyler1;
+	TAdvToolBarOfficeStyler *AdvToolBarOfficeStyler1;
+	TAdvToolBarContainer *AdvToolBarContainer1;
+	TAdvToolBar *AdvToolBar4;
+	TAdvGlowMenuButton *AdvGlowMenuButton1;
+	TAdvToolBar *AdvToolBar5;
+	TAdvOfficeFontSizeSelector *AdvOfficeFontSizeSelector1;
+	TRTTIInspectorBar *RTTIInspectorBar1;
+	TAdvToolPanel *AdvToolPanel1;
+	TAdvToolPanelTab *AdvToolPanelTab1;
+	TAdvToolPanel *AdvToolPanel2;
+	TAdvToolPanel *AdvToolPanel3;
+	TAdvToolPanel *AdvToolPanel4;
+	TAdvToolPanel *AdvToolPanel5;
+	TButton *Button1;
+	void __fastcall btn_TestClick(TObject *Sender);
+
 private:	// User declarations
 public:		// User declarations
 	__fastcall TFormMain(TComponent* Owner);
